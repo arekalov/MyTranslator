99d53b78cc2c5545056e0097a0ece8193c2a1a85
arekalov <artyom.rekalov@gmail.com>
2024-05-21 16:26:12 +0300

    delete comments

--- data/src/main/java/com/arekalov/data/impl/TranslationDataBase.kt
+++ data/src/main/java/com/arekalov/data/impl/TranslationDataBase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.arekalov.data.models.TranslationEntity
 
-@Database(entities = [TranslationEntity::class], version = 1)
+@Database(entities = [TranslationEntity::class], version = 1, exportSchema = false)
 internal abstract class TranslationDataBase : RoomDatabase() {
     abstract fun getProductDao(): TranslationDAO
 }
